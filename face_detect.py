487ba152586c0145bbdb5744fb39a0e73a99f3ad
vishallight <vishallight782@gmail.com>
2019-03-21 13:32:01 -0400

    Add files via upload

---
+++ face_detect.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2,pickle,os
+import webbrowser,pyttsx
+
+cam = cv2.VideoCapture(0)
+face_casade=cv2.CascadeClassifier("data/haarcascade_frontalface_alt2.xml")
+
+recognizer=cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("facetrainner.yml")
+
+	
+	
+label={"person_name":1}
+with open("labels.pickle",'rb') as f:
+	orgin_label=pickle.load(f)
+	label= {v:k for k,v in orgin_label.items()}
+print("\n\n \t \t PRESS 'q' TO EXIT")
+while 1:
+	a,frame=cam.read()
+	
+	gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	face=face_casade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+	for (x,y,w,h) in face:
+		print(x,y,w,h)
+		roi_gray=gray[y:y+h, x:x+h]
+		
+		id_, conf= recognizer.predict(roi_gray)
+		if conf>=45 and conf<=85:
+			print(id_)
+			print(label[id_])
+			font = cv2.FONT_HERSHEY_SIMPLEX
+			name = label[id_]
+			
+			color=(255,255,255)
+			stroke=2
+			cv2.putText(frame, name, (x,y), font,1, color, stroke, cv2.LINE_AA)
+			
+		roi_col=frame#[y:y+h,x:x+w]				#now it capture the full frame when the face is detected
+		img="pic/shalini/img_"+str(x)+".JPG"
+		cv2.imwrite(img,roi_col)		#to capture the face 
+		color=(0,0,255)
+		stroke=2
+		width= x + w	
+		height=y+h
+		
+		cv2.rectangle(frame,(x,y),(width,height),color,stroke)
+	cv2.imshow('STREAM',frame)
+	
+	
+	if cv2.waitKey(1) & 0xFF==ord('q'):
+		break
+cam.release()
+cv2.destroyAllWindows()
---
+++ face_train.py
@@ -0,0 +1,48 @@
+import os,cv2,pickle 
+import numpy as np
+from PIL import Image
+DIR=os.path.dirname(os.path.abspath(__file__))
+img_dir=os.path.join(DIR,"IMG")				#directory of the image 
+face_casade=cv2.CascadeClassifier("data/haarcascade_frontalface_alt2.xml")
+
+recognizer=cv2.face.LBPHFaceRecognizer_create()
+
+x_train=[]
+y_labels=[]
+currt_id=0
+label_id={}
+for root, dirs,files in os.walk(img_dir):
+	for file in files:
+		if file.endswith("png") or file.endswith("JPG") or file.endswith("jpg") or file.endswith("PNG") or file.endswith("jpeg") :
+			path=os.path.join(root,file)
+			label=os.path.basename(os.path.dirname(path)).replace(" ","-").lower()	#path or root
+			#print(label, path)
+			#x_train.append(path)			#show the par
+			#y_labels.append(label)
+			if not label in label_id:
+				label_id[label]=currt_id	
+				currt_id+=1
+			
+			id_=label_id[label]
+			#print(label_id)
+			pill_img=Image.open(path).convert("L")	#gray scale img
+			img_array=np.array(pill_img,"uint8")	#img format
+			#print(img_array)
+			face=face_casade.detectMultiScale(img_array, scaleFactor=1.5, minNeighbors=5)
+			
+			for (x,y,w,h) in face:
+				roi=img_array[y:y+h, x:x+w]
+				x_train.append(roi)
+				y_labels.append(id_)
+#print(y_labels)
+#print(x_train)
+
+with open("labels.pickle",'wb') as f:			#creating and writing the data to a file
+	pickle.dump(label_id, f)
+
+try:
+	recognizer.train(x_train, np.array(y_labels))
+	recognizer.save("facetrainner.yml")						#face trained data
+	print("\n\n\nIMAGE SUCCESSFULLY TRAINED")
+except :
+	print("\n\n\nADD DIFFERENT TYPE OF FACE ON IMG ATLEAST 2")
